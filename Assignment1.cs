45c995fba0c2b47b65a7be43e53f6852bf83bd2f
paufrxn19 <164872421+paufrxn19@users.noreply.github.com>
2024-04-01 19:43:24 +0800

    Add files via upload

---
+++ Assignment1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Assignment_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string username = "Admin";
+            string password = "Admin1234!";
+            int Attempts = 3;
+
+            while (Attempts >= 1)
+            {
+                Console.Write("Username : ");
+                string x = Console.ReadLine();
+
+                Console.Write("Password : ");
+                string y = Console.ReadLine();
+
+                if (x == username && y == password)
+                {
+                    Console.WriteLine("Login was Successful");
+                    break;
+                }
+                else
+                {
+                    Attempts--;
+                    Console.WriteLine("Incorrect. Please try again.");
+                    Console.WriteLine();
+
+                    if (Attempts >= 1)
+                    {
+                        Console.WriteLine(Attempts + " more try");
+                        Console.WriteLine();
+                    }
+                }
+            }
+
+            if (Attempts < 1)
+            {
+                Console.WriteLine("Maximum attempts reached");
+            }
+        }
+    }
+}
+
